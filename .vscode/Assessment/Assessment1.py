21d33dcde0e90e190bba3de82e9fe058fcc8c5b4
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2019-02-20 10:05:38 +1000

    V0.0.1 Dev
    Earliest Build, semi working template (formatting is not updating)

---
+++ .vscode/Assessment/Assessment1.py
@@ -0,0 +1,18 @@
+# serve.py
+
+from flask import Flask  
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    return render_template("something.html")
+
+# run the application
+if __name__ == "__main__":  
+    app.run(debug=True)
+
+
